fca45e56258db8d3d793651b75fea0050ad47cff
sohailaahmed12 <sohaila.hanafy07@eng-st.cu.edu.eg>
2026-07-28 15:36:23 +0300

    added words

--- backend/game/words.js
+++ backend/game/words.js
@@ -1,6 +1,14 @@
 const words = [
   'cat', 'house', 'guitar', 'pizza', 'rainbow',
   'bicycle', 'elephant', 'sandwich', 'volcano', 'umbrella',
+  'dog', 'ice-cream', 'butterfly', 'castle', 'robot',
+  'octopus', 'campfire', 'snowman', 'pirate', 'dragon',
+  'skateboard', 'lighthouse', 'jellyfish', 'volcano-eruption', 'spaceship',
+  'penguin', 'waterfall', 'kite', 'cactus', 'mermaid',
+  'basketball', 'accordion', 'flamingo', 'tornado', 'sunflower',
+  'submarine', 'dinosaur', 'cupcake', 'telescope', 'campground',
+  'unicorn', 'volcano-island', 'treehouse', 'scarecrow', 'chandelier',
+  'hedgehog', 'parachute', 'lightning', 'campfire-story', 'mountain',
 ];
 
 module.exports = words;
